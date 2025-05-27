3a180c43197e61c395d77850abdd54c1856d2873
Endlesscrazz <1209panu@gmail.com>
2025-05-27 11:46:08 -0600

    First commit with basic react app setup

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+
+      <div className='pattern '/>
+
+      <div className='wrapper'>
+        <header>
+          <h1>Find <span className='text-gradient'>Movies</span> that you will Enjoy Without the Hassle</h1>
+        </header>
+
+        <p>Search</p>
+      </div>
+
+
+    </main>
+    
+  )
+}
+
+export default App
